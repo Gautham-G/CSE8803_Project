c5c28f9d4b6c66a9d2d46d330f075748d505f514
Yilun 'Elon' Zha <yilunzha@gatech.edu>
2021-11-30 21:10:00 -0500

    create mapping from ZCTA to CT

--- scripts/data_util.py
+++ scripts/data_util.py
@@ -3,14 +3,30 @@ import json
 
 
 def map_caseRate_ZCTA_to_CT():
+    """
+
+    :return: no return, write a csv under data
+    """
     caseRate = pd.read_csv('../data/caserate-by-modzcta.csv')
-    crosswalk = pd.read_csv('../data/')
 
     # gather column, rename, parse zip code
     caseRate = pd.melt(caseRate, id_vars = 'week_ending')
     caseRate['variable'] = caseRate['variable'].str.split('_', expand = True).loc[:,1]
-    caseRate = caseRate.rename({'variable': 'zip', 'value': 'caseRate'}, axis=1)
+    caseRate = caseRate.rename({'variable': 'ZIP', 'value': 'caseRate'}, axis=1)
+    caseRate.ZIP = caseRate.ZIP.astype('str')
 
     # exclude aggregated statistics row
-    caseRate = caseRate.loc[~caseRate.zip.isin(['SI','QN','MN','BK','BX','CITY']),:]
+    caseRate = caseRate.loc[~caseRate.ZIP.isin(['SI','QN','MN','BK','BX','CITY']),:]
 
+    crosswalk = pd.read_excel('../data/ZIP_TRACT_crosswalk.xlsx')
+    crosswalk['ZIP'] = crosswalk['ZIP'].astype('str').str.pad(5, side='left', fillchar='0')
+
+    # join tables
+    caseRate_cw = caseRate.merge(crosswalk, how = 'left', on = 'ZIP')
+    caseRate_cw['rateShare'] = caseRate_cw['caseRate'] * caseRate_cw['TOT_RATIO']
+
+    # aggregate to census tract
+    caseRateByCT = caseRate_cw.groupby(['week_ending','TRACT']).agg(rate = ('rateShare','sum'))
+    caseRateByCT = caseRateByCT.reset_index()
+
+    caseRateByCT.to_csv('../data/caserate_by_tract.csv', index = False)

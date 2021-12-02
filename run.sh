1dd289912b5018853d16d800ff581bdc0d278a32
kage08 <harshavardhan864.hk@gmail.com>
2021-12-02 00:29:50 -0500

    Change loc of main scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python ./train_scripts/train_model.py -a 1 -e 2000
-python ./train_scripts/train_model.py -a 2 -e 2000
-python ./train_scripts/train_model.py -a 3 -e 2000
-python ./train_scripts/train_model.py -a 4 -e 2000
+python train_model.py -a 4 -e 2000
+python train_model.py -a 2 -e 2000
+python train_model.py -a 3 -e 2000
+python train_model.py -a 1 -e 2000
